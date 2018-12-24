3e49435c040cdf7bdcdeee0999e21cf9f5e1850b
dongchaofeng <dongjialong2006@163.com>
2018-12-24 14:03:27 +0800

    :bug:fix(v1.0): 修改日志
    Signed-off-by: dongchaofeng <dongjialong2006@163.com>

--- godep.go
+++ godep.go
@@ -9,8 +9,10 @@ import (
 
 func main() {
 	var cmd bool = false
+	var version bool = false
 	set := flag.NewFlagSet("godep", flag.ContinueOnError)
 	set.BoolVar(&cmd, "update", false, "update packages accord to glide.yaml file")
+	set.BoolVar(&version, "version", false, "godep version")
 	if err := set.Parse(os.Args[1:]); nil != err {
 		fmt.Println(err)
 		return
@@ -23,7 +25,8 @@ func main() {
 
 		if "update" != os.Args[1] || "up" != os.Args[1] || "-update" != os.Args[1] || "-up" != os.Args[1] {
 			cmd = true
-
+		} else if "version" == os.Args[1] || "-version" == os.Args[1] || "v" == os.Args[1] || "-v" == os.Args[1] {
+			version = true
 		} else {
 			fmt.Println(fmt.Sprintf("unknown command:%s.", strings.Join(os.Args, " ")))
 			return
